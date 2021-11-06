6e42fcec19863e865057d601331241e55e797108
AnthonyKobanda <anthony.kobanda@gmail.com>
2021-11-06 18:29:00 +0100

    running (not opti)

--- main.py
+++ main.py
@@ -1,40 +1,11 @@
-from IPython.display import display
+from torch import nn
 
-import matplotlib.pyplot as plt
-import numpy as np
-import os
 import pandas as pd
-import seaborn as sns
-import sys
-
-from torch import nn, optim
-from torch.autograd.variable import Variable
-from torchvision import transforms, datasets
-
 import torch
-import torch.nn.functional as F
-import torch.utils.data as data_utils
-import torchvision.utils as vutils
 
 
 device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 print("device: {}".format(device))
-
-
-class Disciminator(nn.Module):
-
-    def __init__(self, input_size=4):
-        super(Disciminator, self).__init__()
-        self.fc1 = nn.Linear(input_size, 32)
-        self.nl1 = nn.LeakyReLU(0.25)
-        self.fc2 = nn.Linear(32, 16)
-        self.nl2 = nn.LeakyReLU(0.125)
-        self.fc3 = nn.Linear(16, 1)
-    
-    def forward(self, x):
-        o = self.nl1(self.fc1(x))
-        o = self.nl2(self.fc2(o))
-        return torch.sigmoid(self.fc3(o))
 
 
 class Generator(nn.Module):
@@ -61,4 +32,4 @@ if __name__ == "__main__":
     df = pd.read_csv("noise.csv")
     noise = torch.tensor(df.values).to(device).float()
     outputs = G(noise).cpu().detach().numpy()
-    pd.DataFrame(outputs).to_csv("outputs.csv", index = None)
+    pd.DataFrame(outputs).to_csv("outputs.csv", index=None)
